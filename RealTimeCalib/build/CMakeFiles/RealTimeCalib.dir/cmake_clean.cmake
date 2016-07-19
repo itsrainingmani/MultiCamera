3aa13adc99574e6c84946ee15169bf9433afe793
Manikandan Sundararajan <mtiruman@buffalo.edu>
2016-07-19 09:47:47 -0400

    Added Calibtest to test the real time stereoCalibrate

---
+++ RealTimeCalib/build/CMakeFiles/RealTimeCalib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/RealTimeCalib.dir/RealTimeCalib.cpp.o"
+  "RealTimeCalib.pdb"
+  "RealTimeCalib"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/RealTimeCalib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
